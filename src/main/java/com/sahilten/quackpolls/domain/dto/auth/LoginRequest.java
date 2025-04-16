612ba088dd82a918c3940f2c172a0a72297140c2
Sahil Yadav <10sahil11@gmail.com>
2025-04-16 23:58:21 +0530

    add auth dtos

---
+++ src/main/java/com/sahilten/quackpolls/domain/dto/auth/LoginRequest.java
@@ -0,0 +1,15 @@
+package com.sahilten.quackpolls.domain.dto.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class LoginRequest {
+    private String email;
+    private String password;
+}
