32ac4971703ba96d3a0952fce9364c2ad6379084
Sahil Yadav <10sahil11@gmail.com>
2025-04-17 13:12:36 +0530

    add jwt expiration to QuackpollsApplication.java

--- src/main/java/com/sahilten/quackpolls/QuackpollsApplication.java
+++ src/main/java/com/sahilten/quackpolls/QuackpollsApplication.java
@@ -11,7 +11,7 @@ public class QuackpollsApplication {
         Dotenv dotenv = Dotenv.load();
         System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
         System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
-        System.setProperty("JWT_SECRET", dotenv.get("JWT_SECRET"));
+        System.setProperty("JWT_EXPIRATION", dotenv.get("JWT_EXPIRATION"));
         SpringApplication.run(QuackpollsApplication.class, args);
     }
 
