3ffb30b75a4f5a1769ff4cea45fc01d087c15863
Sahil Yadav <10sahil11@gmail.com>
2025-04-18 01:11:15 +0530

    add crud to poll services

--- src/main/java/com/sahilten/quackpolls/services/PollService.java
+++ src/main/java/com/sahilten/quackpolls/services/PollService.java
@@ -2,6 +2,18 @@ package com.sahilten.quackpolls.services;
 
 import com.sahilten.quackpolls.domain.entities.PollEntity;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 public interface PollService {
     PollEntity save(PollEntity pollEntity);
+
+    Optional<PollEntity> get(UUID pollId);
+
+    PollEntity update(PollEntity pollEntity);
+
+    void delete(UUID pollId);
+
+    List<PollEntity> getAll();
 }
--- src/main/java/com/sahilten/quackpolls/services/impl/PollServiceImpl.java
+++ src/main/java/com/sahilten/quackpolls/services/impl/PollServiceImpl.java
@@ -6,6 +6,10 @@ import com.sahilten.quackpolls.services.PollService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 @Service
 @RequiredArgsConstructor
 public class PollServiceImpl implements PollService {
@@ -16,4 +20,25 @@ public class PollServiceImpl implements PollService {
     public PollEntity save(PollEntity pollEntity) {
         return pollRepository.save(pollEntity);
     }
+
+    @Override
+    public Optional<PollEntity> get(UUID pollId) {
+        return pollRepository.findById(pollId);
+    }
+
+    @Override
+    public PollEntity update(PollEntity pollEntity) {
+        return pollRepository.save(pollEntity);
+    }
+
+    @Override
+    public void delete(UUID pollId) {
+        pollRepository.deleteById(pollId);
+    }
+
+    @Override
+    public List<PollEntity> getAll() {
+        return pollRepository.findAll();
+    }
+
 }
